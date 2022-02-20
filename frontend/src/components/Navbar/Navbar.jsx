57fe199d993248773c8e94f3d86a6616ad0eff33
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-02-20 16:01:47 +0700

    navber completed

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,49 @@
+import { logo } from '../../constants/images';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+
 import './Navbar.scss';
+import { useState } from 'react';
 
 const Navbar = () => {
-  return <nav>
-    <div>
-      
-    </div>
-  </nav>;
+  const [toggle, setToggle] = useState(false);
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => {
+          return (
+            <li key={`link-${item}`} className="app__flex p-text">
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}>
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {['home', 'about', 'work', 'skills', 'contact'].map((item) => {
+                return (
+                  <li key={`link-${item}`} className="app__flex p-text">
+                    <a href={`#${item}`}>{item}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </motion.div>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
