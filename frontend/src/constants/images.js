0b76c2436b7d4ca21202df670cbbf2655059a4c1
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-02-25 16:51:40 +0700

    skills Work footer completed

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,4 +1,4 @@
-import email from '../assets/email.png';
+import emailImage from '../assets/email.png';
 import mobile from '../assets/mobile.png';
 import api from '../assets/api.png';
 import cpp from '../assets/cpp.png';
@@ -36,7 +36,7 @@ import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
 
 export {
-  email,
+  emailImage,
   mobile,
   api,
   cpp,
